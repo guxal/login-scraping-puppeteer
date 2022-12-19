ec4d4bd348f20246881bf8eb8977f539159817ad
guxal <jonathanacp93@gmail.com>
2022-12-19 12:54:14 -0500

    fix login COD

--- index.mjs
+++ index.mjs
@@ -1,19 +1,21 @@
 import puppeteer from 'puppeteer-extra';
 import RecaptchaPlugin from 'puppeteer-extra-plugin-recaptcha';
 import {executablePath} from 'puppeteer'
-//const puppeteer = require('puppeteer-extra')
+import hidden from 'puppeteer-extra-plugin-stealth';
 
 (async  () => {
-     // const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
      puppeteer.use(
           RecaptchaPlugin({
-               provider: { id: '2captcha', token: 'e757141abb212dadb3a9be2daecc3164' },
+               provider: { id: '2captcha', token: '7af124a60f7f8554e0db9d84a9820b99' },
                visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
           })
      )
 
+     puppeteer.use(hidden())
      const browser = await puppeteer.launch({
+          args: ['--no-sandbox'],
           headless: false,
+          ignoreHTTPSErrors: true,
           executablePath: executablePath(),
      });
      const page = await browser.newPage();
@@ -26,20 +28,29 @@ import {executablePath} from 'puppeteer'
      // Type into search box.
      await page.waitForSelector('#username')
           .then(() => console.log('First URL with image: '));
-     await page.type('#username', 'lolo@gmail.com', {"delay": 100});
-     await page.type('#password', 'lololololo', {"delay": 100});
+     await page.type('#username', '<username>', {"delay": 100});
+     await page.type('#password', '<password>', {"delay": 100});
      
      await page.solveRecaptchas()
-
+     
+     
      await Promise.all([
           page.waitForNavigation(),
           page.click(`#login-button`)
      ])
 
-     // Get cookies
      const cookies = await page.cookies();
 
-     console.log(cookies);
+     // console.log(cookies);
 
-  await browser.close();
+     console.log(getCookie(cookies, 'ACT_SSO_COOKIE'))
+
+     await browser.close();
 })();
+
+
+function getCookie(cookies, name){
+     return cookies.filter(
+          function(cookies){ return cookies.name == name}
+     )
+}
